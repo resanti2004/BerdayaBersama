3a82ce669c5ad1aab5f68c1d3af8bf192779dcb9
Resanti Dwi Cahyani <resantidwicahyani@mail.ugm.ac.id>
2025-08-11 15:56:39 +0700

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
         {/* LOGO */}
         <Link to="/" className="flex items-center space-x-1">
           <img src={Logo} className="h-9" alt="Logo" />
-          <span className="self-center text-2xl font-semibold dark:text-white">
+          <span className="self-center text-2xl font-semibold">
             BerdayaBersama
           </span>
         </Link>
